f63c871a88a3cea979c33da1518a381f8cce965b
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2024-08-01 15:58:50 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('form-sorteador').addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt(numeroMaximo);
+
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    })
+})
